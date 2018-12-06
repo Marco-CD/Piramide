51cd7d1b938282b04fe8ab079f5e8da4cab38f86
Marco Cores Dios <marco.cores@gmail.com>
2018-12-06 13:17:24 +0100

    Agregar archivos de proyecto.

---
+++ Piramite/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Piramite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escribir una pirámide con *");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
